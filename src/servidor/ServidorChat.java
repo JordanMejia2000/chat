53afa9640ea2525b59d96c32ddd9aae9609fabfe
JordanMejia2000 <jordanmejiav@gmail.com>
2024-01-25 12:08:44 +0100

    Nuevo commit y nuevo autor

--- src/servidor/ServidorChat.java
+++ src/servidor/ServidorChat.java
@@ -20,7 +20,7 @@ import static util.Util.version;
  * Esta tarea la realiza el hilo asociado a un objeto GestorCliente.
  *
  * @author Jordan Mejia
- * @author Jordan Mejia2
+ * @author Jordan Mejia3
  * @version 0.3.2
  */
 public class ServidorChat {
